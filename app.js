a7a11c73b92dc8632f83d60e9d36c4920301136c
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-11-05 11:58:16 +0800

    add index route

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import indexRoute from './routes.js';
 
 const app = express();
 dotenv.config();
+
+app.use('/', indexRoute);
 
 app.listen(process.env.LOCAL_PORT, (err) => {
   if (err) {
