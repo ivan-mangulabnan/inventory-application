52d702077bcd379675b1fc276f6c2c963ddd3e59
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-11-06 09:34:55 +0800

    add static file directory

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import indexRoute from './routes.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import indexRoute from './routes/index.js';
 
 const app = express();
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filepath);
+const assetsPath = path.join(__dirname, 'public');
 dotenv.config();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static(assetsPath));
 app.use('/', indexRoute);
 
 app.listen(process.env.LOCAL_PORT, (err) => {
