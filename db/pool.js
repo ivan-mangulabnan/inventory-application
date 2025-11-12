dcfeab5e46ace8d915bbb80a4d09e240c7fe73c8
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-11-12 15:26:17 +0800

    only once instance of pool to send. too slow

--- db/pool.js
+++ db/pool.js
@@ -1,3 +1,5 @@
 import { Pool } from "pg";
 
-export default new Pool({ connectionString: process.env.DB_URL });
+const pool = new Pool({ connectionString: process.env.DB_URL });
+
+export default pool;
